3d846f2a719f21cc04350422df67182b827a3923
Ribiveer <55600063+Ribiveer@users.noreply.github.com>
2022-12-22 00:35:41 +0100

    Get initial project set up

---
+++ main.asm
@@ -0,0 +1,11 @@
+;Original disassembly by doppelganger (doppelheathen@gmail.com)
+;edited by Ribiveer (smbasm@ribiveer.com)
+
+INESPRG 2
+INESCHR 1
+INESMAP 0
+INESMIR 1
+
+.INCLUDE "smb.asm"
+
+.INCBIN "Super Mario Bros. (World).nes", $8010
--- smb.asm
+++ smb.asm
@@ -1,24 +1,3 @@
-;SMBDIS.ASM - A COMPREHENSIVE SUPER MARIO BROS. DISASSEMBLY
-;by doppelganger (doppelheathen@gmail.com)
-
-;This file is provided for your own use as-is.  It will require the character rom data
-;and an iNES file header to get it to work.
-
-;There are so many people I have to thank for this, and without their help this would
-;probably not be possible.  So I thank all the peeps in the nesdev scene whose insight into
-;the 6502 and the NES helped me learn how it works (you guys know who you are, there's no 
-;way I could have done this without your help), as well as the authors of x816 and SMB 
-;Utility, and the reverse-engineers who did the original Super Mario Bros. Hacking Project, 
-;which I compared notes with but did not copy from.  Last but certainly not least, I thank
-;Nintendo for creating this game and the NES, without which this disassembly would
-;only be theory.
-
-;Update: removed residual note under ChkStart label.  Thanks to ShaneM for pointing out
-;the error.
-
-;Assembles with x816.
-
-;-------------------------------------------------------------------------------------
 ;DEFINES
 
 ;NES specific hardware defines
