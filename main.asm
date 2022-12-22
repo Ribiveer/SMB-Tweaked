eb6360074f79225379b26333bbda8705e65c9c94
Ribiveer <55600063+Ribiveer@users.noreply.github.com>
2022-12-22 01:34:13 +0100

    Split constants, defines and music data.

--- main.asm
+++ main.asm
@@ -6,6 +6,20 @@ INESCHR 1
 INESMAP 0
 INESMIR 1
 
+.INCLUDE "nes-defines.asm"
+.INCLUDE "game-defines.asm"
+.INCLUDE "constants.asm"
+
+.org $8000              ; Makes sure that the addresses used have the proper offsets.
+
 .INCLUDE "smb.asm"
+
+.INCLUDE "music-data.asm"
+
+;INTERRUPT VECTORS
+      .PAD $FFFA
+      .dw NonMaskableInterrupt
+      .dw Start
+      .dw $fff0  ;unused
 
 .INCBIN "Super Mario Bros. (World).nes", $8010
